075c7c03038408cc875a4c11d61ee25a292a37ee
deproxies <deproxies@gmail.com>
2026-02-24 22:50:37 -0600

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,4 +1,4 @@
-local BaseURL = "https://raw.githubusercontent.com/deproxies/Main/refs/heads/main/"
+local BaseURL = "https://raw.githubusercontent.com/deproxies/Deproxware/refs/heads/main/"
 
 local Loadstrings = {
     [119987266683883] = BaseURL .. "Brainrotshit.lua",
@@ -8,6 +8,6 @@ for place, url in Loadstrings do
     if place == game.PlaceId then
         loadstring(game:HttpGet(url))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/deproxies/Main/refs/heads/main/UniversalAimbot.lua"))()
+        loadstring(game:HttpGet(BaseURL .. "UniversalAimbot.lua"))()
     end
 end
